45a76005704c7ba95e9ed2cd60533c3ee4213f49
Sebastian <sanctuary@yandex.ru>
2018-03-19 00:11:37 +0500

    реализованы и перегружены сложение, вычитание, умножение, вывод

--- ooplab2-enterthematrix/Source.cpp
+++ ooplab2-enterthematrix/Source.cpp
@@ -6,72 +6,112 @@ using namespace std;
 
 class Matrix {
 	float **f_p_matrix;
-	int i_size, i_length;
+	int i_size;
 public:
-	Matrix(int i_size = 5): i_size(i_size), i_length(i_size*i_size){
+	//конструктор по умолчанию + основной
+	Matrix(int i_size = 5): i_size(i_size) {
 		int i, j;
 		srand(time(0));
 		f_p_matrix = new float *[i_size];
 		for (i = 0; i < i_size; i++) f_p_matrix[i] = new float[i_size];
 		for (i = 0; i < i_size; i++)
 			for (j = 0; j < i_size; j++)
-				f_p_matrix[i][j] = rand() % 20 + 1;
+				f_p_matrix[i][j] = rand() % 11;
 	}
 
-	Matrix(const Matrix &matrix) {
-		i_size = matrix.i_size;
-		Matrix *temp = new Matrix[i_size];
-		for (int i = 0; i < i_size; i++)
-			for (int j = 0; j < i_size; j++)
-				temp->f_p_matrix[i][j] = matrix.f_p_matrix[i][j];
-	}
-	
-	float Get(int i, int j) {
-		return f_p_matrix[i][j];
+	//оператор сложения
+	Matrix& operator + (const Matrix& matrix) {
+		return Sum(matrix);
 	}
 
-	void Put(int i, int j, float f_value) {
-		f_p_matrix[i][j] = f_value;
+	//оператор вычитания
+	Matrix& operator - (const Matrix& matrix) {
+		return Sub(matrix);
 	}
 
-	void Show() {
-		int i, j;
-		cout << "\nРазмер матрицы - " << i_size << "x" << i_size << ", количество элементов - " << i_length;
-		for (i = 0; i < i_size; i++) {
-			cout << endl << endl;
-			for (j = 0; j < i_size; j++)
-				cout << setw(2) << f_p_matrix[i][j] << " ";
-		}
-		cout << endl << endl;
+	//оператор умножения
+	Matrix& operator * (const Matrix& matrix) {
+		return Mul(matrix);
 	}
 
-	Matrix operator+(Matrix &matrix)
-	{
-		Matrix *result = new Matrix(i_size);
-		for (int i = 0; i < i_size; ++i)
-			for (int j = 0; j < i_size; ++j)
-				result->f_p_matrix[i][j] = this->f_p_matrix[i][j] + matrix.f_p_matrix[i][j];
-		result->Show();
-		return *result;
-	}
-
+	Matrix(const Matrix&); //конструктор копирования
+	Matrix& Sum(const Matrix& matrix); //метод сложения
+	Matrix& Sub(const Matrix& matrix); //метод вычитания
+	Matrix& Mul(const Matrix& matrix); //метод умножения
+	friend ostream& operator<<(ostream&, Matrix&); //оператор вывода
 };
+
+//конструктор копирования
+Matrix::Matrix(const Matrix& matrix) {
+	i_size = matrix.i_size;
+	Matrix *temp = new Matrix[i_size];
+	for (int i = 0; i < i_size; i++)
+		for (int j = 0; j < i_size; j++)
+			temp->f_p_matrix[i][j] = matrix.f_p_matrix[i][j];
+}
+
+//метод сложения
+Matrix& Matrix::Sum(const Matrix& matrix) {
+	Matrix *result = new Matrix(i_size);
+	for (int i = 0; i < i_size; ++i)
+		for (int j = 0; j < i_size; ++j)
+			result->f_p_matrix[i][j] = f_p_matrix[i][j] + matrix.f_p_matrix[i][j];
+	return *result;
+}
+
+//метод вычитания
+Matrix& Matrix::Sub(const Matrix& matrix) {
+	Matrix *result = new Matrix(i_size);
+	for (int i = 0; i < i_size; ++i)
+		for (int j = 0; j < i_size; ++j)
+			result->f_p_matrix[i][j] = f_p_matrix[i][j] - matrix.f_p_matrix[i][j];
+	return *result;
+}
+
+//метод умножения
+Matrix& Matrix::Mul(const Matrix& matrix) {
+	Matrix *result = new Matrix(i_size);
+	int i, j, k;
+
+	for (i = 0; i < i_size; i++)
+		for (j = 0; j < i_size; j++)
+		{
+			result->f_p_matrix[i][j] = 0;
+			for (k = 0; k < i_size; k++)
+				result->f_p_matrix[i][j] += (f_p_matrix[i][k] * matrix.f_p_matrix[k][j]);
+		}
+	return *result;
+}
+
+//оператор вывода
+ostream& operator <<(ostream& out, Matrix& matrix) {
+	int i, j;
+	for (i = 0; i < matrix.i_size; i++) {
+		out << endl;
+		for (j = 0; j < matrix.i_size; j++)
+			out << setw(3) << matrix.f_p_matrix[i][j] << " ";
+	}
+	out << endl << endl << endl;
+	return out;
+}
 
 void main() {
 	setlocale(LC_ALL, "russian");
 	cout << "КВАДРАТНЫЕ МАТРИЦЫ 1.0\n";
-	int i;
-	cout << "Введите размер матрицы: ";
-	cin >> i;
-	Matrix *m_1 = new Matrix(i), *m_2 = m_1, *m_3 = new Matrix();
-	cout << "\nПервая матрица.";
-	m_1->Show();
-	cout << "Вторая матрица.";
-	m_2->Show();
-	m_3->Show();
-	*m_3 = *m_1 + *m_2;
-	m_3->Show();
-
+	int i_size;
+	cout << "Введите размерность: ";
+	cin >> i_size;
+	Matrix *m_1 = new Matrix(i_size),
+		*m_2 = m_1,
+		*m_res = new Matrix(i_size);
+	cout << "\nОригинальная матрица:\n" << *m_1;
+	cout << "Скопированная матрица:\n" << *m_2;
+	*m_res = *m_1 + *m_2;
+	cout << "Матрица1 + Матрица2:\n" << *m_res;
+	*m_res = *m_1 - *m_2;
+	cout << "Матрица1 - Матрица2:\n" << *m_res;
+	*m_res = *m_1 * *m_2;
+	cout << "Матрица1 * Матрица2:\n" << *m_res;
 	cout << endl << endl;
 	system("@pause");
 }
