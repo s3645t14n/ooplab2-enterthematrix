44451b4a799270d87532440189264741f027d668
Sebastian <sanctuary@yandex.ru>
2018-03-19 10:12:18 +0500

    билд который я покажу преподу

--- ooplab2-enterthematrix/Source.cpp
+++ ooplab2-enterthematrix/Source.cpp
@@ -1,4 +1,14 @@
-﻿#include <iostream>
+﻿//Определить класс «Квадратная матрица» – Matrix. Класс должен 
+//содержать несколько конструкторов, в том числе конструктор 
+//копирования.Реализовать методы для сложения, вычитания, 
+//умножения матриц; вычисления нормы матрицы.Перегрузить 
+//операции сложения, вычитания, умножения и присваивания для 
+//данного класса.Создать массив объектов класса Matrix и 
+//передать его в функцию, которая изменяет i - ю матрицу 
+//путем возведения ее в квадрат.В головной программе вывести 
+//результат.
+
+#include <iostream>
 #include <iomanip>
 #include <ctime>
 
@@ -8,16 +18,17 @@ class Matrix {
 	float **f_p_matrix;
 	int i_size;
 public:
-	//конструктор по умолчанию + основной
-	Matrix(int i_size = 5): i_size(i_size) {
-		srand(time(0));
+	//конструктор по умолчанию
+	Matrix() {
+		cout << "Введите размерность: ";
+		cin >> i_size;
 		f_p_matrix = new float *[i_size];
 		for (int i = 0; i < i_size; i++) f_p_matrix[i] = new float[i_size];
 		for (int i = 0; i < i_size; i++)
 			for (int j = 0; j < i_size; j++)
 				f_p_matrix[i][j] = rand() % 11;
 	}
-
+	
 	//оператор сложения
 	Matrix& operator + (const Matrix& matrix) {
 		return Sum(matrix);
@@ -33,10 +44,12 @@ public:
 		return Mul(matrix);
 	}
 
+	Matrix(int i_size); //конструктор основной
 	Matrix(const Matrix&); //конструктор копирования
 	Matrix& Sum(const Matrix& matrix); //сложение
 	Matrix& Sub(const Matrix& matrix); //вычитание
 	Matrix& Mul(const Matrix& matrix); //умножение
+	Matrix& operator = (const Matrix& matrix); //оператор присваивания
 	float InfNorm(); //бесконечная норма
 	float FstNorm(); //первая норма
 	float EucNorm(); //евклидова норма
@@ -46,10 +59,19 @@ public:
 //конструктор копирования
 Matrix::Matrix(const Matrix& matrix) {
 	i_size = matrix.i_size;
-	Matrix *temp = new Matrix[i_size];
+	f_p_matrix = new float *[i_size];
+	for (int i = 0; i < i_size; i++) f_p_matrix[i] = new float[i_size];
 	for (int i = 0; i < i_size; i++)
 		for (int j = 0; j < i_size; j++)
-			temp->f_p_matrix[i][j] = matrix.f_p_matrix[i][j];
+			f_p_matrix[i][j] = matrix.f_p_matrix[i][j];
+}
+
+Matrix::Matrix(int i_size) : i_size(i_size) {
+	f_p_matrix = new float *[i_size];
+	for (int i = 0; i < i_size; i++) f_p_matrix[i] = new float[i_size];
+	for (int i = 0; i < i_size; i++)
+		for (int j = 0; j < i_size; j++)
+			f_p_matrix[i][j] = rand() % 11;
 }
 
 //метод сложения
@@ -81,6 +103,14 @@ Matrix& Matrix::Mul(const Matrix& matrix) {
 				result->f_p_matrix[i][j] += (f_p_matrix[i][k] * matrix.f_p_matrix[k][j]);
 		}
 	return *result;
+}
+
+//оператор присваивания
+Matrix& Matrix::operator = (const Matrix& matrix) {
+	for (int i = 0; i < i_size; i++)
+		for (int j = 0; j < i_size; j++)
+			f_p_matrix[i][j] = matrix.f_p_matrix[i][j];
+	return *this;
 }
 
 //нахождение бесконечной нормы
@@ -128,26 +158,49 @@ ostream& operator <<(ostream& out, Matrix& matrix) {
 	return out;
 }
 
+//вывод массива матриц
+void PrintMas(int i_quan, Matrix *matrix) {
+	cout << "\nMatrix *m_mas = new Matrix[i_quan]:\n";
+	for (int i = 0; i < i_quan; i++)
+	cout << "m_mas[" << i << "]" << matrix[i];
+}
+
+//возведение матрицы в квадрат по индексу
+void Pow(int i_num, Matrix *matrix) {
+	matrix[i_num] = matrix[i_num] * matrix[i_num];
+}
+
 void main() {
 	setlocale(LC_ALL, "russian");
+	srand(time(0));
 	cout << "КВАДРАТНЫЕ МАТРИЦЫ\n";
-	int i_size;
-	cout << "Введите размерность: ";
+	int i_size, i_quan, i_num;
+	cout << "Введите размерность(i_size): ";
 	cin >> i_size;
-	Matrix *m_1 = new Matrix(i_size),
-		*m_2 = m_1,
-		*m_res = new Matrix(i_size);
-	cout << "\nОригинальная матрица:\n" << *m_1;
-	cout << "Бесконечная норма = " << m_1->InfNorm() << endl;
-	cout << "Первая норма = " << m_1->FstNorm() << endl;
-	cout << "Евклидова норма = " << m_1->EucNorm() << endl << endl;
-	cout << "Скопированная матрица:\n" << *m_2;
-	*m_res = *m_1 + *m_2;
-	cout << "Матрица1 + Матрица2:\n" << *m_res;
-	*m_res = *m_1 - *m_2;
-	cout << "Матрица1 - Матрица2:\n" << *m_res;
-	*m_res = *m_1 * *m_2;
-	cout << "Матрица1 * Матрица2:\n" << *m_res;
+	Matrix m_1(i_size),
+		m_2 = m_1,
+		m_res = (i_size);
+	cout << "\nОригинальная матрица (Matrix m_1(i_size)):\n" << m_1;
+	cout << "Бесконечная норма (m_1.InfNorm()) = " << m_1.InfNorm() << endl;
+	cout << "Первая норма (m_1.FstNorm()) = " << m_1.FstNorm() << endl;
+	cout << "Евклидова норма (m_1.EucNorm()) = " << m_1.EucNorm() << endl << endl;
+	cout << "Скопированная матрица (конструктор копирования, m_2 = m_1):\n" << m_2;
+	m_res = m_1 + m_2;
+	cout << "Матрица1 + Матрица2 (m_res = m_1 + m_2):\n" << m_res;
+	m_res = m_1 - m_2;
+	cout << "Матрица1 - Матрица2 (m_res = m_1 - m_2):\n" << m_res;
+	m_res = m_1 * m_2;
+	cout << "Матрица1 * Матрица2 (m_res = m_1 * m_2):\n" << m_res;
+	m_res = m_1;
+	cout << "Матрица3 = Матрица1 (m_res = m_1): \n" << m_res;
+	cout << "Введите число матриц в массиве (i_quan):";
+	cin >> i_quan;
+	Matrix *m_mas = new Matrix[i_quan];
+	PrintMas(i_quan, m_mas);
+	cout << "Введите номер матрицы для возведения в квадрат:";
+	cin >> i_num;
+	Pow(i_num, m_mas);
+	PrintMas(i_quan, m_mas);
 	cout << endl << endl;
 	system("@pause");
 }
